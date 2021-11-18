43b62f795cbc286703feed599063ae6ee35d0d48
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-17 21:21:44 -0600

    inital commit.

---
+++ problem3.js
@@ -0,0 +1,18 @@
+
+
+/*const addRow = () => {
+	const table = document.getElementById("myTable");
+	let row = table.insertRow();
+	let data = "row 3 data 1"
+
+	row.innerText = data;
+}*/
+
+
+function addRow() {
+	const table = document.getElementById("myTable");
+	let row = table.insertRow();
+	let data = "row 3 data 1"
+
+	row.innerText = data;
+}
