2c5faf16d2cacf0389be98a5301be5ae8e4ffb1e
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-19 19:44:56 -0600

    update.

--- problem3.js
+++ problem3.js
@@ -9,10 +9,19 @@
 }*/
 
 
-function addRow() {
+/*function addRow() {
 	const table = document.getElementById("myTable");
 	let row = table.insertRow();
 	let data = "row 3 data 1"
 
 	row.innerText = data;
+}*/
+
+ function addRow() {
+ const table = document.getElementById("myTable");
+  let row = table.insertRow(0);
+  let data1 = row.insertCell(0);
+  let data2 = row.insertCell(1);
+  data1.innerHTML = "New data1";
+  data2.innerHTML = "New data2";
 }
