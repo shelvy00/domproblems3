5a09114f76b82db1f81236b493fe2c74cd39346a
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-21 13:56:28 -0600

    update.

--- problem3.js
+++ problem3.js
@@ -19,9 +19,9 @@
 
  function addRow() {
  const table = document.getElementById("myTable");
-  let row = table.insertRow(0);
-  let data1 = row.insertCell(0);
-  let data2 = row.insertCell(1);
+ const row = table.insertRow(0);
+ const data1 = row.insertCell(0);
+ const data2 = row.insertCell(1);
   data1.innerHTML = "New data1";
   data2.innerHTML = "New data2";
 }
